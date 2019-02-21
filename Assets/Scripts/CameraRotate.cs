3457321e57872de6250c58556256dc56ba51811d
ckrise <calebkrise@gmail.com>
2019-02-20 19:02:46 -0600

    Main Menu Recreate

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // https://forum.unity.com/threads/rotate-the-camera-around-the-object.47353/
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
