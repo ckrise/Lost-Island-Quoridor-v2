6cba95a3901b8fa86088a13dae31c782e6c67dd0
jaltman1 <38510712+jaltman1@users.noreply.github.com>
2019-02-24 18:19:05 -0600

    Revert "More AI Optimizations"
    This reverts commit d2eaa3efc01efb8d01a691e33459f5f4ceb071a1.

--- Assets/Scripts/AI/TreeNode.cs
+++ Assets/Scripts/AI/TreeNode.cs
@@ -44,16 +44,14 @@ public class TreeNode
 
         int p2Column = board.GetPlayerTwoPos()[0]; //Ascii column value of a-i
         int p2Row = board.GetPlayerOnePos()[1]; //Ascii row value of 1-9
-        HashSet<string> wallsOfInterest = new HashSet<string>();
+        List<string> wallsOfInterest = new List<string>();
 
         List<int> columnsOfInterest = new List<int> { p1Column - 1, p1Column, p1Column + 1, p2Column - 1, p2Column, p2Column + 1 };
         List<int> rowsOfInterest = new List<int> { p1Row - 1, p1Row, p1Row + 1, p2Row - 1, p2Row, p2Row + 1 };
         List<string> toBeRemoved = new List<string>();
 
         foreach (string wall in board.GetWallsPlaced()) {
-            foreach (string importantWall in DictionaryLookup.PerformWallsOfInterestLookup(wall)) {
-                wallsOfInterest.Add(importantWall);
-            }
+            wallsOfInterest.AddRange(DictionaryLookup.PerformWallsOfInterestLookup(wall));
         }
 
         foreach (string move in moves) {
