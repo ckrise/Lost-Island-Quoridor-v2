53404aaf17dab7fad86a14739b619a79c0896f8c
ckrise <calebkrise@gmail.com>
2019-03-22 18:12:43 -0500

    Menu Button Animations

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private float scale = 1.25f;
+    private RectTransform rectTransform;
+    public float normalWidth = 200;
+    public float normalHeight = 25;
+    private float HoverWidth { get => normalWidth * scale; }
+    private float HoverHeight { get => normalHeight * scale; }
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Debug.Log(rectTransform);
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, HoverWidth);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, HoverHeight);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, normalWidth);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, normalHeight);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, normalWidth);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, normalHeight);
+    }
+}
