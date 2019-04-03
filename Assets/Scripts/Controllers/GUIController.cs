400078fbbf666b638763ed21e2504456ac70c378
bradleyacutts <bradleyacutts@gmail.com>
2019-04-02 19:56:03 -0500

    Brad's sticky note bug fixes

--- Assets/Scripts/Controllers/GUIController.cs
+++ Assets/Scripts/Controllers/GUIController.cs
@@ -476,6 +476,7 @@ public class GUIController : MonoBehaviour
             musicReference.playWin();
             if (GameData.InAdventureMode)
             {
+                GameData.AdventureProgress++;
                 adventureWinPanel.SetActive(true);
             }
             else
@@ -668,13 +669,13 @@ public class GUIController : MonoBehaviour
     {
         if (GameData.AdventureProgress == 1)
         {
-            GameData.AdventureProgress++;
+            
             GameData.AIDifficulty = "intermediate";
             levelLoader.GetComponent<LevelLoader>().LoadLevel("JungleScene");
         }
         else if (GameData.AdventureProgress == 2)
         {
-            GameData.AdventureProgress++;
+            
             GameData.AIDifficulty = "hard";
             levelLoader.GetComponent<LevelLoader>().LoadLevel("TempleScene");
         }
