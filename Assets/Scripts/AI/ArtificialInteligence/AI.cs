1f8d1820a2712dac8d7367de296de8f3cd927be9
jaltman1 <38510712+jaltman1@users.noreply.github.com>
2019-04-02 19:23:04 -0500

    Update AI.cs

--- Assets/Scripts/AI/ArtificialInteligence/AI.cs
+++ Assets/Scripts/AI/ArtificialInteligence/AI.cs
@@ -86,6 +86,7 @@ namespace ArtificialInteligence
         //Initiates a minimax search 2 layers deep.
         public string GetHardMove(string playerMove)
         {
+            TreeNode.weights = new List<float> { 1f, 1f, .25f, 0f };
             HandlePlayerMove(playerMove);
 
             TreeNode rootNode = new TreeNode(CurrentBoard);
